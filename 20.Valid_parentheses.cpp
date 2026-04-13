746e2da785805326302285ca82f3c31f125b3b3f
hoegg825-byte <hoegg825@gmail.com>
2026-04-13 22:55:19 +0800

    Create 20.Valid_parentheses.cpp

---
+++ 20.Valid_parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c:s){
+            if (c == ')'){
+                if (!st.empty() && st.top() == '(') st.pop();
+                else return false;
+            }
+            else if (c == '}'){
+                if (!st.empty() && st.top() == '{') st.pop();
+                else return false;
+            }
+            else if (c == ']'){
+                if (!st.empty() && st.top() == '[') st.pop();
+                else return false;
+            }
+            else {
+                st.push(c);
+            }
+        }
+        if (st.empty()) return true;
+        return false;
+    }
+};

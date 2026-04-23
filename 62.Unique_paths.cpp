6cac0fd4ee1443461b3315bb734cd1441ba80a51
hoegg825-byte <hoegg825@gmail.com>
2026-04-23 23:18:52 +0800

    Create 62.Unique_paths.cpp

---
+++ 62.Unique_paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        return combinations(m+n-2, m-1);
+    }
+    int combinations(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    if (k == 0 || k == n) return 1;
+    if (k > n / 2) k = n - k;
+
+    long long res = 1;
+    for (int i = 1; i <= k; ++i) {
+        res = res * (n - i + 1) / i;
+    }
+    return res;
+    }
+};

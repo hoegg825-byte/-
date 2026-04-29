09d202df6eb9a20e6323a364ddcbf238572419dc
hoegg825-byte <hoegg825@gmail.com>
2026-04-29 23:07:24 +0800

    Create 80.Remove_duplicates_from_sorted_array_2.cpp

---
+++ 80.Remove_duplicates_from_sorted_array_2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return n;
+        int l = 2, r = 2;
+        while (r < n){
+            if (nums[l-2] != nums[r]){
+                nums[l] = nums[r];
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+};
